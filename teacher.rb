895490e19cb2410e548c7d4359a2a979c24278f9
mongare <mongaresams@gmail.com>
2022-03-03 18:49:09 +0300

    Add teacher subclass that extends person

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,14 @@
 require_relative 'person.rb'
 
 class Teacher < Person
-    attr_accessor :classroom
-
+    attr_accessor :specialization
+    def initialize (age, name = "Unknown", parent_permission = true, specialization)
+        super(age, name, parent_permission)
+        @specialization = specialization
+    end
+    def specialization=(specialization)
+        @specialization = specialization
+    end
     def can_use_services?
         return true
     end
